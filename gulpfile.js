89b4646e27fd9fc53188f23861e324dcd4004e02
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2016-11-15 22:37:51 -0800

    Intitial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function () {
+    return gulp.src('./app/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('style', function () {
+    return gulp.src('./app/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js', function() {
+    return gulp.src('./app/js/main.js')
+        .pipe(gulp.dest('./js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('home', function() {
+    return gulp.src('./app/*.html')
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './app/'
+        }
+    });
+
+    // gulp.watch('./app/main.js', ['js']);
+    gulp.watch('./app/scss/*.scss', ['styles', 'style']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'style', 'js', 'home', 'serve']);
