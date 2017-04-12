97251fe3824b47cc9e8707ae74ec5fad9c85cf4d
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2017-04-12 00:58:46 -0700

    Added some new styling for the navbar
    * Updated the navbar to use an animation when scrolling the page

--- app/js/main.js
+++ app/js/main.js
@@ -17,17 +17,25 @@ $('.front').mouseleave(function() {
   $(this).css("box-shadow", "0 0 1em white");
 }); 
 
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
-});
-
 $('.album-flipper').click(function () {
   $(".front").fadeToggle(1000, 0);
   $(".back").toggleClass('hide');
   $(".back").toggleClass('show');
+});
+
+$(function(){
+  var shrinkHeader = 300;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+    if ( scroll >= shrinkHeader ) {
+      $('.main-header').addClass('shrink');
+      $('nav').css("height", "75px");
+      // alert("added class of shrink!");
+    } else {
+      $('.main-header').removeClass('shrink');
+    }
+  });
+  function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
 });
