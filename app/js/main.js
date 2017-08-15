ca69e828ef703b7134d980b6efad22ca8bbcafc4
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2017-08-14 21:58:07 -0700

    Fixed navbar image issue, etc
    * Fixed the scrolling issue for the navbar image
    * Deleted some commented out code
    * Fixed the order of the albums in the dropdown menu

--- app/js/main.js
+++ app/js/main.js
@@ -35,7 +35,7 @@ function update(){
         var $element = $(this);
         // subtract some from the height b/c of the padding
         var height = $element.height()-18;
-        $(this).css('backgroundPosition', '50% ' + Math.round(-pos * velocity) + 'px'); 
+        $(this).css('backgroundPosition', '0% ' + Math.round(-pos * velocity) + 'px'); 
     }); 
 };
 
